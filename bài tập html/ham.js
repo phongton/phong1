24d4ddb07c06e3cc9b3b3937acb6ef3ce798d633
unknown <phongton219@gmail.com>
2024-04-21 17:34:52 +0700

    up 2

---
+++ bài tập html/ham.js
@@ -0,0 +1,27 @@
+console.log(bien.indexOf(""))//Tìm vị trí của mảng
+console.log(bien.lastIndexOf(""))//tìm vij trí cuối của trong biến
+console.log(bien.search(""))//tìm vij trí trong biến
+console.log(bien.slice("tham so 1 , tham so 2 "))//cắt vị trí của tham số trongbieens
+console.log(bien.replace("tham so muon doi ","tham so dc thay the"))// đổi 1 giá trị trong bến sang giá trị khác
+
+
+
+console.log(mang.join(''))// thêm 1 phần tử giữa các phần tử trog mảng
+console.log(mang.pop())// xoá phaanf tử cuoi mảng và trả về phần tử đã xoá
+console.log(mang.push(''))// thêm 1 hoặc nhiều  phần tử vào cuối mảng và trả về độ dài mới của mảng
+console.log(mang.shift())//xoá phânf tử đầu tieen của mảng
+console.log(mang.unshift())//thêm 1 hoă nhiều phần tử vào đầu mảng và trả về độ dài mới của mảng
+console.log(mang.splice("vị trí của phần tử cần xoá","chọn xoá bao nhiêu phần tử trong mảng","có thể trỳeeenf 1 phần tử khác vào"))
+console.log(mang.concat("mảng bạn muốn lieen kết "))
+console.log(mang.slice("vị trí của phần tử muốn cắt",'chọn cắt bao nhiêu phần tử i'))// cắt phần tử trong mảng
+
+
+
+
+
+
+
+
+
+
+
